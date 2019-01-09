6d38e22644acdfa8d182d55f3efbff299c5329ed
Victoria Martinez <vmartinez72@live.com>
2019-01-08 21:31:26 -0800

    add babel to file and add package to json file

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,17 @@ module.exports = {
        test: /\.js$/,
        exclude: /node_modules/,
        loader: "eslint-loader"
+     },
+     {
+       test: /\.js$/,
+       exclude: [
+         /node_modules/,
+         /spec/
+       ],
+       loader: "babel-loader",
+       options: {
+         presets: ['es2015']
+       }
      }
    ]
  }
